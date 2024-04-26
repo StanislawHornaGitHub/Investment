0ded0a26c7467c260bf3a2a01a54cf3c68e2fec9
Stanislaw Horna <stanislawhorna@outlook.com>
2024-04-26 16:06:24 +0200

    update input var type

--- Flask/Utility/ConvertToDict.py
+++ Flask/Utility/ConvertToDict.py
@@ -21,7 +21,7 @@ from SQL.Fund import Fund
 class ConvertToDict:
     
     @staticmethod
-    def fundList(fund: dict[str, Fund]):
+    def fundList(fund: list[Fund]):
         result = {}
         for f in fund:
             result[f.getFundID()] = f
