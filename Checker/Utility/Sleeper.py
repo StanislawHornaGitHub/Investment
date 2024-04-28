d663338a1b7c832b2464a2257ee1da86c3a472e2
Stanislaw Horna <stanislawhorna@outlook.com>
2024-04-28 22:58:00 +0200

    add stop signals to stop container without waiting for timeout

--- Checker/Utility/Sleeper.py
+++ Checker/Utility/Sleeper.py
@@ -15,16 +15,20 @@
     Date            Who                     What
 
 """
-
+import time
+import sys
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
-import time
+
+from Utility.Terminator import Terminator
 
 
 @dataclass
 class Sleeper:
 
     TimeInterval_ms: int = field(init=True, default=3600000)
+
+    __TERMINATOR: Terminator = field(init=False, default=Terminator())
 
     __LastCheckIn: datetime = field(init=False)
     __NextCheckIn: datetime = field(init=False)
@@ -52,21 +56,53 @@ class Sleeper:
         )
         self.checkIn()
         return TimeToSleep
-    
+
+    def __invoke_sleep(self, sleepTime: float):
+
+        fractionTime = sleepTime % 1
+        fullSeconds = int(sleepTime)
+
+        time.sleep(fractionTime)
+
+        for c in range(0, fullSeconds):
+            if self.__TERMINATOR.getStatus() == True:
+                exit(0)
+
+            time.sleep(1)
+
     def checkIn(self) -> None:
         self.__last_check_in()
         self.__next_check_in()
-        
+
         return None
+
+    def getTimeSinceLastCheckIn(self) -> float:
+        return (
+            (
+                datetime.now() - self.__LastCheckIn
+            )
+            .total_seconds()
+        )
 
     def start(self) -> float:
 
+        OperationTime = self.getTimeSinceLastCheckIn()
         TimeToSleep = self.__calculate_time_to_sleep()
 
         if TimeToSleep <= 0:
-            print("Sleep time exceeded by ", (TimeToSleep * -1), " seconds")
+            print(
+                "Operations took: ",
+                OperationTime, " seconds. ",
+                "Sleep time exceeded by ",
+                (TimeToSleep * -1), " seconds."
+            )
         else:
-            print("Sleep for: ", TimeToSleep, " seconds")
-            time.sleep(TimeToSleep)
+            print(
+                "Operations took: ",
+                OperationTime, " seconds. ",
+                "Sleep for: ",
+                TimeToSleep, " seconds"
+            )
+            self.__invoke_sleep(TimeToSleep)
 
         return TimeToSleep
