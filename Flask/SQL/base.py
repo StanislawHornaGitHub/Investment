d25705207d6aeb22edf83a781df563dead105d6a
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-01 20:59:22 +0200

    bugfix DB port

--- Flask/SQL/base.py
+++ Flask/SQL/base.py
@@ -22,7 +22,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 IP = os.getenv('DB_IP_Address', 'localhost')
-PORT = os.getenv('DB_Port', '5532')
+PORT = os.getenv('DB_Port', '5432')
 USERNAME = os.getenv('DB_Username', 'api_write')
 PASSWORD = os.getenv('DB_Password', 'inv!w_ap_ite2')
 DB_NAME = os.getenv('DB_Name', 'Investments')
