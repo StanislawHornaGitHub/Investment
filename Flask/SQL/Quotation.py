8b7193787b7ba6c5a40348a52fb539ecd210735d
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-05 22:02:34 +0200

    add f_keys mapping

--- Flask/SQL/Quotation.py
+++ Flask/SQL/Quotation.py
@@ -5,7 +5,7 @@
 
 .NOTES
 
-    Version:            1.0
+    Version:            1.1
     Author:             Stanisław Horna
     Mail:               stanislawhorna@outlook.com
     GitHub Repository:  https://github.com/StanislawHornaGitHub/Investment
@@ -13,23 +13,34 @@
     ChangeLog:
 
     Date            Who                     What
+    2024-05-05      Stanisław Horna         Add Foreign keys mapping.
 
 """
 
-from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
+from sqlalchemy import orm, Column, String, Integer, Float, DateTime, ForeignKey
 
 from SQL.base import Base
+
 
 class Quotation(Base):
     __tablename__ = 'quotations'
 
     date = Column(DateTime, primary_key=True)
-    fund_id = Column(Integer, ForeignKey("funds.fund_id"), primary_key=True)
+    fund_id = Column(
+        Integer,
+        ForeignKey("funds.fund_id"),
+        primary_key=True
+    )
     value = Column(Float)
     daily_change = Column(Float)
     weekly_change = Column(Float)
     monthly_change = Column(Float)
     yearly_change = Column(Float)
+
+    Fund = orm.relationship(
+        "Fund",
+        back_populates="Quotation"
+    )
 
     def __init__(self, q_date, fund_id, value, daily_change=None, weekly_change=None, monthly_change=None, yearly_change=None):
         self.date = q_date
@@ -39,6 +50,6 @@ class Quotation(Base):
         self.weekly_change = weekly_change
         self.monthly_change = monthly_change
         self.yearly_change = yearly_change
-        
+
     def downloadNewQuotation(self):
         pass
